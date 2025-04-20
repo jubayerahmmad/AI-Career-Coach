7e403d26813a86c58656b3c6734987a23067feca
jubayerahmmad <zubayersami510@gmail.com>
2025-04-20 11:08:18 +0600

    setup project and clerk implementation

---
+++ src/app/layout.jsx
@@ -0,0 +1,44 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/ui/theme-provider";
+import Header from "@/components/Header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "SENSAI - AI CAREER COACH",
+  description:
+    "An AI-powered career coaching platform to guide you towards success.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en" suppressHydrationWarning>
+        <body className={`${inter.className} `}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {/* header */}
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            {/* Footer */}
+            <footer className="bg-muted/50 py-6 text-center text-gray-200">
+              <p>Made with ❤️ By Jubayer Ahmmad</p>
+            </footer>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
